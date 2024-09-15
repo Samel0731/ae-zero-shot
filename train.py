feat(train): fix loss calculation and save loss plot

train.py
@@ -1,11 +1,12 @@
 import argparse
 import os
 import torch
-import sys
+from math import ceil
 from matplotlib import pyplot as plt
 from model import build_AE, test_model_architecture
 from torch.utils.data import DataLoader
 from torchvision import transforms
+from torchvision.transforms import v2
 from tqdm import tqdm
 from utils.loader import KFoldDataset
 # PyTorch TensorBoard support
@@ -42,7 +43,7 @@ def main():
     EPOCHS = 5
 
     best_vloss = 1_000_000.
-    lr = 0.001
+    lr = 0.0001
     # --------------------------------------------------------------------------
 
     # --------------------------------------------------------------------------
@@ -50,14 +51,16 @@ def main():
     batch_size = 32
     transform = transforms.Compose([
         transforms.Resize((200,200)),   # Resize images to 200x200
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandAugment(),
         transforms.ToTensor(),  # Convert images to tensor (values between 0 and 1)
     ])
     training_set = KFoldDataset(root_dir=dataset_root, field_names=field_names, k=0, train=True, transform=transform)
     validation_set = KFoldDataset(root_dir=dataset_root, field_names=field_names, k=0, train=False, transform=transform)
     training_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True)
     validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=False)
-    iters_per_epoch = len(training_set) // batch_size
-    viters_per_epoch = len(validation_set) // batch_size
+    iters_per_epoch = ceil(len(training_set) / batch_size)
+    viters_per_epoch = ceil(len(validation_set) / batch_size)
     # --------------------------------------------------------------------------
 
     training_epoch_loss = []
@@ -79,33 +82,26 @@ def main():
         train_loss = 0.
         running_loss = 0.
         last_loss = 0.
-        pbar = tqdm(enumerate(training_loader),desc=f"Epoch: [{epoch+1}/{EPOCHS}] Iter: [{0}/{iters_per_epoch}]", total=iters_per_epoch)
+        pbar = tqdm(enumerate(training_loader), desc=f"Epoch: [{epoch+1}/{EPOCHS}] Iter: [{0}/{iters_per_epoch}]", total=iters_per_epoch)
         for iter, (inputs, labels) in pbar:
             optimizer.zero_grad()
             inputs = inputs.to(device)
             labels = labels.to(device)
             outputs = autoencoder(inputs)
-            print('check shape',outputs.shape, labels.shape)
             loss = loss_fn(outputs, labels)
             loss.backward()
             optimizer.step()
         
-            running_loss += loss.item()
             train_loss += loss.item()
-            if iter % 1000 == 999:
-                last_loss = running_loss / 1000
-                tb_x = epoch * len(training_loader) + iter + 1
-                writer.add_scalar('Loss/train', last_loss, tb_x)
-                running_loss = 0.
             pbar.set_description(f"Epoch(Training): [{epoch+1}/{EPOCHS}] Iter: [{iter+1}/{iters_per_epoch}] Loss: {train_loss / (iter+1):.8f}")
-        train_loss /= len(training_set)
+        train_loss /= len(training_loader)
         training_epoch_loss.append(train_loss)
-        avg_loss = last_loss
+        avg_loss = train_loss
         # --------------------------------------------------------------------------
 
         running_vloss = 0.
         autoencoder.eval()
-        vbar = tqdm(enumerate(validation_loader),total=len(validation_set),desc=f"Epoch(Validating):[{epoch+1}/{EPOCHS}]")
+        vbar = tqdm(enumerate(validation_loader), desc=f"Epoch(Validating):[{epoch+1}/{EPOCHS}]", total=viters_per_epoch)
         viter_number = 0
         with torch.no_grad():
             for viter, vdata in vbar:
@@ -114,13 +110,13 @@ def main():
                 vlabels = vlabels.to(device)
                 voutputs = autoencoder(vinputs)
                 vloss = loss_fn(voutputs, vlabels)
-                running_vloss += vloss
+                running_vloss += vloss.item()
                 vbar.set_description(f"Epoch(Validating): [{epoch+1}/{EPOCHS}] Iter: [{viter+1}/{viters_per_epoch}] Loss: {running_vloss / (viter+1):.8f}")
                 viter_number = viter
 
-        avg_vloss = running_vloss / (viter_number + 1)
+        avg_vloss = running_vloss / len(validation_loader)
         validation_epoch_loss.append(avg_vloss)
-        print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+        print('LOSS train {:.8f} valid {:.8f}'.format(avg_loss, avg_vloss))
 
         writer.add_scalars('Training vs. Validation Loss',
                     { 'Training' : avg_loss, 'Validation' : avg_vloss },
@@ -139,7 +135,7 @@ def main():
     plt.plot(training_epoch_loss, label='train_loss')
     plt.plot(validation_epoch_loss,label='val_loss')
     plt.legend()
-    plt.show
+    plt.savefig('loss_curve.png', dpi=300)
 
 
 if __name__=='__main__':